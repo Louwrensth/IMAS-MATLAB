c1ba6695d1f2fa061b2aa6bbeedd9cc8a94688a8
Antoine Merle <antoine.merle@epfl.ch>
2019-04-30 09:01:07 +0200

    Remove un-necessary files.

--- tests/testPutGet.m
+++
@@ -1,7 +0,0 @@
-function testPutGet(testCase, path)
-  idx = testCase.TestData.idx;
-  ids = testCase.TestData.IDS.(path);
-  ids_put(idx,path,ids);
-  sdi = ids_get(idx,path,0);
-  comparator(ids,sdi,path);
-end
--- tests/testPutSliceGet.m
+++
@@ -1,8 +0,0 @@
-function testPutSliceGet(testCase, path)
-  idx = testCase.TestData.idx;
-  ids_slice = testCase.TestData.IDS_slice.(path);
-  ids_put_non_timed(idx,path,ids_slice);
-  ids_put_slice(idx,path,ids_slice);
-  sdi = ids_get(idx,path);
-  comparator(ids_slice,sdi,path);
-end
