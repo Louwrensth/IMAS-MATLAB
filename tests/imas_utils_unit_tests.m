2a41de51d6126aafe4bef366fba4814331e1f6f9
Antoine Merle <antoine.merle@epfl.ch>
2019-10-04 10:17:26 +0200

    Adapt imas_utils_unit_tests to the new definition of ids_rand.

--- tests/imas_utils_unit_tests.m
+++ tests/imas_utils_unit_tests.m
@@ -19,8 +19,8 @@ classdef imas_utils_unit_tests < matlab.unittest.TestCase
   methods (Test)
 
     function rand(TestCase, IDSname, ntime)
-      ids1 = ids_rand(IDSname, ntime, false);
-      ids2 = ids_rand(IDSname, ntime, true );
+      ids1 = ids_rand(IDSname, ntime, 0);
+      ids2 = ids_rand(IDSname, ntime, 2);
     end
 
     function gen(TestCase, IDSname)
@@ -28,29 +28,29 @@ classdef imas_utils_unit_tests < matlab.unittest.TestCase
     end
 
     function int_to_double(TestCase, IDSname, ntime)
-      ids = ids_rand(IDSname, ntime, false);
+      ids = ids_rand(IDSname, ntime, 0);
       sdi = ids_int_to_double(IDSname, ids);
     end
     
     function double_to_int(TestCase, IDSname, ntime)
-      ids = ids_int_to_double(IDSname, ids_rand(IDSname, ntime, false));
+      ids = ids_int_to_double(IDSname, ids_rand(IDSname, ntime, 0));
       sdi = ids_double_to_int(IDSname, ids);
     end
     
     function empty_to_nan(TestCase, IDSname, ntime)
-      ids = ids_rand(IDSname, ntime, false);
+      ids = ids_rand(IDSname, ntime, 0);
       sdi = ids_empty_to_nan(IDSname, ids);
     end
     
     function nan_to_empty(TestCase, IDSname, ntime)
-      ids = ids_rand(IDSname, ntime, false);
+      ids = ids_rand(IDSname, ntime, 0);
       sdi = ids_nan_to_empty(IDSname, ids);
     end
     
     function cell_to_struct(TestCase, IDSname, ntime)
       params = imas_get_mex_params;
       imas_set_mex_params('use_cell_array_for_array_of_structures',true);
-      ids = ids_rand(IDSname, ntime, false);
+      ids = ids_rand(IDSname, ntime, 0);
       sdi = ids_cell_to_struct(IDSname, ids);
       imas_set_mex_params(params);
     end
@@ -58,7 +58,7 @@ classdef imas_utils_unit_tests < matlab.unittest.TestCase
     function struct_to_cell(TestCase, IDSname, ntime)
       params = imas_get_mex_params;
       imas_set_mex_params('use_cell_array_for_array_of_structures',false);
-      ids = ids_rand(IDSname, ntime, false);
+      ids = ids_rand(IDSname, ntime, 0);
       sdi = ids_struct_to_cell(IDSname, ids);
       imas_set_mex_params(params);
     end
