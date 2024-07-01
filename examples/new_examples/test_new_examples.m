81ce639d34b3b1132dcbcaec8ff024bfe8534e18
Daniel Figat <dfigat@man.poznan.pl>
2024-07-01 11:42:24 +0200

    First draft of example_002 [IMAS-5141]

--- examples/new_examples/test_new_examples.m
+++ examples/new_examples/test_new_examples.m
@@ -1,9 +1,13 @@
 % example001
 example_001_open_database
-    %create_db_entry_legacy
-    %open_db_entry_uri
-    %create_db_entry_uri_with_paths
+    % create_db_entry_legacy
+    % open_db_entry_uri
+    % create_db_entry_uri_with_paths
 
 % example002
+example_002_fill_data_in_ids
+    % creating_completly_new_ids
+    % default_values_and_aos_operationss
+    % copying_and_validating_ids
 % example003
 % example004
