264219d6de61d7aa523c49ba625bb77600561cc9
Daniel Figat <dfigat@man.poznan.pl>
2024-07-03 11:08:24 +0200

    Minor changes and clean ups of example_003 [IMAS-5147]

--- examples/new_examples/example_003_write_data_into_entry.m
+++ examples/new_examples/example_003_write_data_into_entry.m
@@ -66,7 +66,6 @@
         % NOTE: time-independent data is being put only if it is empty in entry
         % In this case summary/stationary_phase_flag/source will be put only at first iteration.
         % Suggested way to fill this type of fields is to do this outside loop
-        %summary.stationary_phase_flag.source = 
         summary.stationary_phase_flag.source = strcat('Name saved by example code iteration: ', num2str(x));
  
         % Fill example data
@@ -91,7 +90,7 @@
     ids_put_slice(ctx, 'summary', summary);
     clear summary;
 
-    % IDSs can be printed using fprintf() and dump() methods.
+    % IDSs can be printed using fprintf() and disp() methods.
     fprintf('Dumping summary from fprintf() and disp() functions\n');
     summary_check = ids_get(ctx, 'summary');
     fprintf('\tsummary.ids_properties.homogeneous_time: %i\n', summary_check.ids_properties.homogeneous_time);
@@ -112,7 +111,6 @@
 %    ╚══════════════════════════════════════════════════════════════════════════════╝
 % This example focuses on putting IDS into another occurrence
 
-    mode = 43;
     ctx = imas_open('imas:mdsplus?path=./testdb_mdsplus', mode);
     
     % default occurrence for get/put is 0
@@ -144,7 +142,7 @@
     % NOTE: there is ids_properties/occurrence_type structure
     % it stores additional information about specific occurrence
 
-    % IDSs can be printed using fprintf() and dump() method
+    % IDSs can be printed using fprintf() method
     fprintf('Dumping equilibrium (occurence 1) from put_another_occurence() function\n');
     equilibrium_check = ids_get(ctx, 'equilibrium', 1);
     fprintf('\tequilibrium.ids_properties.homogeneous_time: %i\n', equilibrium_check.ids_properties.homogeneous_time);
@@ -153,7 +151,7 @@
     fprintf('\tequilibrium.vacuum_toroidal_field.b0:        %i %i %i\n', equilibrium_check.vacuum_toroidal_field.b0);
 
     % occurrences can be listed with imas_list_all_occurrences() function
-    % list_all_occurrences also returns content of IDS pointed by node_path argument
+    % imas_list_all_occurrences also returns content of IDS pointed by node_path argument
     [occurrence_list, node_content_list] = imas_list_all_occurrences(ctx, 'equilibrium', 'vacuum_toroidal_field/r0');
     
     fprintf('occurence list: [%i %i]\n ', occurrence_list);
