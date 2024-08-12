03239dc897af4167a0ea2693d0f4ad0a30f818a1
Maarten Sebregts <msebregts@ignitioncomputing.com>
2024-08-12 09:29:04 +0200

    Add documentation for FLEXBUFFERS_SERIALIZER_PROTOCOL

--- matlab/imas_serialize.m
+++ matlab/imas_serialize.m
@@ -2,17 +2,18 @@
 %
 % Serialize the contents of this IDS into binary data.
 %
-% While it is by design allowed to specify various serialization
-% protocols, it currently implements only a serialization through usage of
-% the ASCII backend (simpler but less efficient) which is de-facto the
-% default serializer protocol. The ID of the used serializer protocol is
-% kept in the serialized buffer, such that specifying the protocol is not
-% necessary when deserializing.
+% There are currently two different serialization protocols. The ASCII protocol
+% serializes the data though the ASCII backend. This is a simpler human readable
+% protocol, but it's also less efficient than the (newer) Flexbuffers protocol.
+% The latter is the default and should be preferred.
+%
+% The ID of the used serializer protocol is kept in the header of the serialized
+% buffer, such that specifying the protocol is not necessary when deserializing.
 %
 % Args:
 %   ids:        IDS to serialize.
 %   ids_name:   Name of the ids.
-%   protocol:   Protocol type (default: ASCII_SERIALIZER_PROTOCOL = 60)
+%   protocol:   Protocol type (default: DEFAULT_SERIALIZER_PROTOCOL)
 %
 % Example:
 %   .. code-block:: matlab
@@ -26,4 +27,6 @@
 %       % memory communication, then deserialize
 %       pf_active2 = imas_deserialize(data, 'pf_active')
 
-ASCII_SERIALIZER_PROTOCOL= 60;
+ASCII_SERIALIZER_PROTOCOL = 60;
+FLEXBUFFERS_SERIALIZER_PROTOCOL = 61;
+DEFAULT_SERIALIZER_PROTOCOL = 61
