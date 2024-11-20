d59e869ccfcb17257be3436ba2db8e316fd38f7c
Ludovic Fleury <ludovic.fleury@cea.fr>
2024-11-20 11:20:00 +0100

    IMAS-3885: updating documentation

--- matlab/ids_get_sample.m
+++ matlab/ids_get_sample.m
@@ -6,11 +6,20 @@
 %
 % 1. In case of no interpolation in the time range, interp must be set to 0 and dtime = [].
 %
+%    This mode returns an IDS object with all constant/static data filled. The
+%    dynamic data is retrieved for the provided time range [tmin, tmax].
+%
 % 2. The method can interpolate time slices in the time range, if interp is not set to 0 and dtime = [step] (double list of size equals 1) where 'step' is the constant time between two slices. 
 %
+%    This mode will generate an IDS with a homogeneous time vector [tmin, tmin + dtime, tmin + 2*dtime, ...] up to tmax. The chosen interpolation
+%    method will have no effect on the time vector, but may have an impact on the other dynamic values. 
+%    The returned IDS always has 'ids_properties.homogeneous_time = 1'.
+%    
 % 3. Interpolation of dynamic data on an explicit time base. This method is selected when dtime and interp are provided. dtime must be a double list of size larger than 1.
 %
-%    This mode will generate an IDS with a homogeneous time vector equals to dtime. tmin and tmax are ignored in this mode.
+%    This mode will generate an IDS with a homogeneous time vector equal to dtime. tmin and tmax are ignored in this mode.
+%    The chosen interpolation method will have no effect on the time vector, but may have an impact on the other dynamic values. The returned IDS always has
+%    'ids_properties.homogeneous_time = 1'.
 %
 % Empty fields within the IDS in the Data Entry are returned with default values.
 % 
